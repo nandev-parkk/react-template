1d90d64cc3b71820813bcf0889eb8abf8b69f645
shpark <shpark@in2wise.com>
2023-03-14 10:09:48 +0900

    add testComponent

---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface TestComponentProps {
+  name: string;
+  age: number;
+}
+
+export default function TestComponent({ name, age }: TestComponentProps) {
+  return (
+    <div>
+      <span>{name}</span>
+      <span>{age}</span>
+    </div>
+  );
+}
